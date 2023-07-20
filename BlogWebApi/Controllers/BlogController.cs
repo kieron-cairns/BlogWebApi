cafb3603158d18816d638a9d3ce48822c6769d6d
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-20 19:35:01 +0100

    CHANGE: Controller method updated access html content from serialized json object

--- BlogWebApi/Controllers/BlogController.cs
+++ BlogWebApi/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text;
+using System.Text.Json;
 using System.Xml;
 
 namespace BlogWebApi.Controllers
@@ -15,12 +16,19 @@ namespace BlogWebApi.Controllers
         {
             using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
             {
-                string htmlContent = await reader.ReadToEndAsync();
+                string jsonString = await reader.ReadToEndAsync();
                 try
                 {
+                    JsonDocument doc = JsonDocument.Parse(jsonString);
+                    string htmlContent = doc.RootElement.GetProperty("content").GetString();
+
                     XmlDocument xmlDoc = new XmlDocument();
                     xmlDoc.LoadXml(htmlContent);
                     return StatusCode(200);
+                }
+                catch (JsonException)
+                {
+                    return StatusCode(400, "Invalid JSON");
                 }
                 catch (XmlException)
                 {
