a58791438f2ed32b0e202655c72e6a5c5e995cbe
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-20 17:54:52 +0100

    CHANGE: Blog controller implemented

---
+++ BlogWebApi/Controllers/BlogController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+       
+    }
+}
