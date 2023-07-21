bb15602c9cd2d5d9f6bb98a6fcc3fcaec7b88ae2
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-21 17:48:36 +0100

    CHANGE: DbContext class file and interface added in preperation for entity framework core sql migration

---
+++ BlogWebApi/Data/BlogPostContext.cs
@@ -0,0 +1,24 @@
+﻿using BlogWebApi.Interfaces;
+using BlogWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogWebApi.Data
+{
+    public class BlogPostContext : DbContext, IBlogPostContext
+    {
+        public DbSet<BlogPost> Posts { get; set; } = null!;
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ BlogWebApi/Interfaces/IBlogPostContext.cs
@@ -0,0 +1,12 @@
+﻿using BlogWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogWebApi.Interfaces
+{
+    public interface IBlogPostContext
+    {
+        DbSet<BlogPost> Posts { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
