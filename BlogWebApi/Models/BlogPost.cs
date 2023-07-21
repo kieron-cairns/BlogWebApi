874152f41a81ba72de64cbbd643143a2f3f1f0c0
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-21 17:24:04 +0100

    CHANGE: Blog HTML data model added in preperation for enetiy framework core sql migration

---
+++ BlogWebApi/Models/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogWebApi.Models
+{
+    public class BlogPost
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string BlogHtml { get; set; }
+
+    }
+}
